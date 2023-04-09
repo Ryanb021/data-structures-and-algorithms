18924b6a212d6e64819fc5a3afa6b7794ae6318d
Ryan Bagan <darcknight1980@gmail.com>
2023-04-08 18:01:01 -0700

    updated

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -7,4 +7,46 @@ describe('Linked List', () => {
   it('works', () => {
     expect(true).toBeTruthy();
   });
+
+  it('should insert at the beginning of empty', () => {
+    const list = new LinkedList();
+    list.IntersectionObserverEntry('playstation1');
+
+    expect(list.head.value).toEqual('playstation1');
+    expect(list.head.next).toBeNull();
+  });
+
+  it('should insert at the beginning of populated list', () => {
+    const list  = new LinkedList();
+    list.insert('playstation1');
+    list.insert('playstation2');
+
+    expect(list.head.value).toEqual('playstation2');
+    expect(list.head.next.value).toEqual('playstation1');
+    expect(list.head.next).toBeNull();
+  });
+
+  it('should display a string properly', () => {
+    const list = new LinkedList();
+    list.insert('playstation1');
+    list.insert('playstation2');
+
+    const linkedString = list.toString();
+
+    expect(linkedString).toEqual('{ playstation2} -> { playstation1} -> NULL');
+  });
+
+  it('should indicate whether the input value exists in the list', () => {
+    const list = new LinkedList();
+    list.insert('playstation1');
+    list.insert('playstation2');
+    list.insert('playstation3');
+    list.insert('playstation4');
+    list.insert('playstation5');
+
+    expect(list.includes('playstation1')).toBeTruthy();
+    expect(list.includes('playstation2')).toBeTruthy();
+    expect(list.includes('playstation6')).toBeFalsy();
+  });
+
 });
