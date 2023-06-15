b4c7a85c0f9e0570c4819c9afb2e831134ab35d9
Ryan Bagan <darcknight1980@gmail.com>
2023-06-15 03:54:29 -0700

    made changes

--- javascript/balanced-tree/index.js
+++ javascript/balanced-tree/index.js
@@ -2,6 +2,7 @@ function isBalanced(root) {
   // Helper function to calculate the height of a node
   function getHeight(node) {
     if (node === null) {
+      // BASE CASE
       return 0;
     }
     return Math.max(getHeight(node.left), getHeight(node.right)) + 1;
@@ -10,6 +11,7 @@ function isBalanced(root) {
   // Helper function to check if the tree is balanced
   function checkBalanced(node) {
     if (node === null) {
+      // BASE CASE
       return true;
     }
 
@@ -30,6 +32,23 @@ function isBalanced(root) {
   return checkBalanced(root);
 }
 
+const node1 = { left: null, right: null, value: 1 };
+const node2 = { left: null, right: null, value: 2 };
+const node3 = { left: null, right: null, value: 3 };
+const node4 = { left: null, right: null, value: 4 };
+const node5 = { left: null, right: null, value: 5 };
+const node6 = { left: null, right: null, value: 6 };
+const node7 = { left: null, right: null, value: 7 };
+node1.right = node2;
+node1.left = node3;
+node2.left = node4;
+node2.right = node5;
+node3.left = node6;
+node3.right = node7;
+
+const root = node1;
+
+
 // Call the function to check if the tree is balanced
 const isTreeBalanced = isBalanced(root);
-console.log(isTreeBalanced); // Output: true
+console.log(isTreeBalanced); // Output: true for balanced or false if not balanced
